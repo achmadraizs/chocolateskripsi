f78de97246702d230bbccf36d5f48984885d345d
achmadraizs <achmadraizsikrillah17@gmail.com>
2020-10-07 13:10:20 +0800

    first commit

---
+++ frontend/src/data.js
@@ -0,0 +1,43 @@
+export default {
+    products:[{
+        _id:'1',
+        name: 'Choco Chocolate Black',
+        category: 'Black',
+        image: '/images/d1.jpg',
+        price: 60,
+        brand: ' Magelang',
+        rating: 4.5,
+        numReviews: 10
+    },
+    {
+        _id:'2',
+        name: 'Choco White Black',
+        category: 'Black',
+        image: '/images/d1.jpg',
+        price: 60,
+        brand: ' Magelang',
+        rating: 4.5,
+        numReviews: 10
+    },
+    {
+        _id:'3',
+        name: 'Choco Pink White',
+        category: 'White',
+        image: '/images/d1.jpg',
+        price: 60,
+        brand: ' Magelang',
+        rating: 4.5,
+        numReviews: 10
+    },
+    {
+        _id:'4',
+        name: 'Choco Green White',
+        category: 'White',
+        image: '/images/d1.jpg',
+        price: 60,
+        brand: ' Magelang',
+        rating: 4.5,
+        numReviews: 10
+    },
+]
+}
